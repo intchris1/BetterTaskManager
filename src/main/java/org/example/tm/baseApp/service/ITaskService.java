92943384de9d98c85cfcb452d97a925a6d459b06
Kristina <daphna11@mail.ru>
2020-05-28 10:44:30 +0300

    Merge branch 'springing-data'

--- src/main/java/org/example/tm/baseApp/service/ITaskService.java
+++ src/main/java/org/example/tm/baseApp/service/ITaskService.java
@@ -2,6 +2,7 @@ package org.example.tm.baseApp.service;
 
 import org.example.tm.entity.Task;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -11,4 +12,5 @@ public interface ITaskService extends IUsersEntityService<Task> {
 
     void deleteAllByProjectId(@NotNull String userId, @NotNull String projectId);
 
+    void attachTask(@NotNull String userId, @Nullable String taskName, @NotNull String projectId) throws Exception;
 }
--- src/main/java/org/example/tm/service/TaskUsersEntityServiceImpl.java
+++ src/main/java/org/example/tm/service/TaskUsersEntityServiceImpl.java
@@ -5,6 +5,7 @@ import org.example.tm.baseApp.repository.ITaskRepository;
 import org.example.tm.baseApp.service.ITaskService;
 import org.example.tm.entity.Task;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,6 +26,15 @@ public class TaskUsersEntityServiceImpl extends AbstractUsersEntityServiceImpl<T
     @Override
     public void deleteAllByProjectId(@NotNull String userId, @NotNull String projectId) {
         rep.deleteAllByUserIdAndProjectId(userId, projectId);
+    }
+
+    @Override
+    public void attachTask(@NotNull String userId, @Nullable String taskName, @NotNull String projectId) throws Exception {
+        if(taskName == null || taskName.isEmpty())
+            throw new Exception("Name is empty");
+        @NotNull final Task task = findByName(userId, taskName);
+        task.setProjectId(projectId);
+        rep.save(task);
     }
 }
 
